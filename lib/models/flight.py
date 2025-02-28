2080898b2bd00ab4ce6e3dfae2cfc80bc9262057
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-28 14:59:36 -0500

    Added __init__() method for Flight model

--- lib/models/flight.py
+++ lib/models/flight.py
@@ -1,4 +1,9 @@
 from models.__init__ import CONN, CURSOR
 
 class Flight:
-    pass
+    
+    def __init__(self, airline, price, origin, destination):
+        self.airline = airline
+        self.price = price
+        self.origin = origin
+        self.destination = destination
