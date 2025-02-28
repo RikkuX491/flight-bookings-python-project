9d9d2cf64c83cd4567a7c08d7e076a94ba795003
Ricardo Guerra <RikkuG491@gmail.com>
2025-02-28 15:08:46 -0500

    Added properties for Flight model

--- lib/models/flight.py
+++ lib/models/flight.py
@@ -7,3 +7,26 @@ class Flight:
         self.price = price
         self.origin = origin
         self.destination = destination
+
+    @property
+    def airline_getter(self):
+        return self._airline
+    
+    @airline_getter.setter
+    def airline(self, value):
+        if (type(value) == str) and (len(value) > 4):
+            self._airline = value
+        else:
+            raise Exception("Error: Airline must be a string that is at least 5 characters long!")
+        
+    @property
+    def price_getter(self):
+        return self._price
+    
+
+    @price_getter.setter
+    def price(self, value):
+        if (type(value) in [int, float]) and (value > 0):
+            self._price = value
+        else:
+            raise Exception("Price must be a number that is greater than 0!")
